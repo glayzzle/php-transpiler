992a96d54c56cecbaa92f399510bdc6c73cbac00
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-21 17:12:38 +0100

    handle primitives

---
+++ src/ast/primitive.js
@@ -0,0 +1,36 @@
+/*!
+ * Copyright (C) 2017 Glayzzle (BSD3 License)
+ * @authors https://github.com/glayzzle/php-transpiler/graphs/contributors
+ * @url http://glayzzle.com
+ */
+'use strict';
+
+var AST = require('../ast');
+
+/**
+ * @constructor AST/Primitive
+ */
+var Primitive = AST.extends(function(parent, node) {
+  AST.apply(this, [parent]);
+  this.node = node;
+});
+
+/**
+ * Outputs the primitive
+ */
+Primitive.prototype.toString = function () {
+  if (this.node.kind === 'string') {
+    return '\'' + node.value
+      .replace(/\n/g, '\\n')
+      .replace(/\r/g, '\\r')
+      .replace(/'/g, '\\\'')
+      + '\'';
+  } else if (this.node.kind === 'integer') {
+    return this.node.value;
+  } else if (this.node.kind === 'boolean') {
+    return this.node.value ? 'true' : 'false';
+  }
+  throw new Error('Undefined primitive type : ' + this.node.kind);
+};
+
+module.exports = Primitive;
