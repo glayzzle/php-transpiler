62d83dcecef9cb5893fcf530e53b2f7028bce375
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-21 17:15:50 +0100

    serialize primitives

---
+++ src/visitor/primitive.js
@@ -0,0 +1,13 @@
+/*!
+ * Copyright (C) 2017 Glayzzle (BSD3 License)
+ * @authors https://github.com/glayzzle/php-transpiler/graphs/contributors
+ * @url http://glayzzle.com
+ */
+'use strict';
+
+/**
+ * Visits a primitive
+ */
+module.exports = function (node, state, output) {
+  output.append('primitive', node);
+};
