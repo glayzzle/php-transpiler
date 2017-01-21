7f8e1bc0a1ef71eb6b175e7c27ddf467a03f39ac
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-21 17:14:45 +0100

    handle echo/print statements

---
+++ src/visitor/echo.js
@@ -0,0 +1,17 @@
+/*!
+ * Copyright (C) 2017 Glayzzle (BSD3 License)
+ * @authors https://github.com/glayzzle/php-transpiler/graphs/contributors
+ * @url http://glayzzle.com
+ */
+'use strict';
+
+/**
+ * Visits the echo/print node
+ */
+module.exports = function (node, state, output) {
+  this.visit(
+    node.arguments,
+    state,
+    output.append('statement').append('call', '$php.stdout')
+  );
+};
