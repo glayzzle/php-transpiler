bec7f69034ac3420f3bec0b02c183ed325349f3b
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-24 10:29:45 +0100

    complete sample

--- test/fibo.php
+++ test/fibo.php
@@ -4,8 +4,16 @@ namespace foo\bar {
   $a = 1;
   $b = 2;
   $c = $a + $b;
+  /**
+   * The famous fibonnaci benchmark to test function calls
+   */
   function fibo(int $a) : int {
-    return $a < 1 ? 1 : fibo($a - 2) + fibo($a - 1);
+    // return $a < 1 ? 1 : fibo($a - 2) + fibo($a - 1);
+    if ($a < 1) {
+      return 1; /* here */
+    } else {
+      return fibo($a - 2) + fibo($a - 1);
+    }
   }
   echo fibo(30);
 }
