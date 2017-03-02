1dfb34323b2fac88755f17fdf7e68dc72e1647e2
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-03-02 15:56:17 +0100

    handle function resolution from objects & static classes

--- src/visitor/call.js
+++ src/visitor/call.js
@@ -9,9 +9,41 @@
  * Visits the echo/print node
  */
 module.exports = function (node, state, output) {
-  var fnName = state.getFunction(
-    state.resolve(node.what, false)
-  );
+  var fnName;
+  if (node.what.kind === 'identifier') {
+    fnName = state.getFunction(
+      state.resolve(node.what, false)
+    );
+  } else if (node.what.kind === 'staticlookup') {
+    fnName = state.getClass(
+      state.resolve(node.what.what, false)
+    );
+    if (node.what.offset.kind === 'constref') {
+      fnName += '.' + node.what.offset.name;
+    } else {
+      throw new Error(
+        'Unable to handle call offset from ' +
+        node.what.offset.kind
+      );
+    }
+  } else if (node.what.kind === 'propertylookup') {
+    state.scope().variable(node.what.what.name);
+    fnName = node.what.what.name;
+    if (node.what.offset.kind === 'constref') {
+      fnName += '.' + node.what.offset.name;
+    } else {
+      throw new Error(
+        'Unable to handle call offset from ' +
+        node.what.offset.kind
+      );
+    }
+  } else {
+    console.log(node.what);
+    throw new Error(
+      'Unable to handle calls from ' +
+      node.what.kind
+    );
+  }
   this.visit(
     node.arguments,
     state,
